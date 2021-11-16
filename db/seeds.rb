85687193567d0d9b0bcdafc2d536c19ad8fb9b80
hans tys <the.hans.tys@gmail.com>
2021-11-16 13:53:32 +0100

    fixed routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+tina = User.new(
+  first_name: "tina",
+  last_name: "gouws",
+  email: "christine.gouws@gmail.com"
+)
+tina.save
+
+# hans = User.new(
+
+# )
