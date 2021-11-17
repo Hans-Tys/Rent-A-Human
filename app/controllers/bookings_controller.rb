0f170ce77b136df08a97fac7f8bb65463ac9440a
hans tys <the.hans.tys@gmail.com>
2021-11-17 22:01:44 +0100

    change a lot of styling cuz of feedback

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,17 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!
   def new
     @booking = Booking.new
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(params_booking)
+    @booking.user_id = current_user
+    @booking.persona_id = @booking.persona
+    @booking.save
+    redirect_to personas_path
+    authorize @booking
   end
 
   def destory
