7cb784bbdc9709ca4b2d7341ee1659a40bc1b6f3
Layla de Waegh <layladewaegh@hotmail.com>
2021-11-18 12:12:36 +0100

    reviews

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, only: [:new, :create, :destroy] do
-      resources :reviews, only: [:new, :create]
+      resources :reviews, only: [:index, :new, :create]
     end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
