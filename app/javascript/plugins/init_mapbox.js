0634589a7d5f9818f0ba11a81e733ef138d4f7d7
christine gouws <christine.gouws@gmail.com>
2021-11-18 16:59:28 +0100

    update

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,11 +5,7 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
+
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -17,18 +13,53 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/christine-gouws/ckw3jfm1n154j14nwo6rjse7z'
     });
+
     const markers = JSON.parse(mapElement.dataset.markers);
+    const mapMarkers = [];
     markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([marker.lng, marker.lat])
-        .addTo(map);
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+      const newMarker = new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup) // add this
+      .addTo(map);
+      mapMarkers.push(newMarker)
+      newMarker.getElement().dataset.markerId = marker.id;
+      newMarker.getElement().addEventListener('click', (e) => toggleCardHighlighting(e));
+      // newMarker.getElement().addEventListener('mouseleave', (e) => toggleCardHighlighting(e));
     });
     fitMapToMarkers(map, markers);
+    openInfoWindow(mapMarkers);
     map.addControl(new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl
     }));
   }
 };
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const openInfoWindow = (markers) => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card, index) => {
+    card.addEventListener('mouseenter', () => {
+      markers[index].togglePopup();
+    });
+    card.addEventListener('mouseleave', () => {
+      markers[index].togglePopup();
+    });
+  });
+}
+
+const toggleCardHighlighting = (event) => {
+  // We select the card corresponding to the marker's id
+  console.log(document.querySelector(`[data-persona-id="${event.currentTarget.dataset.markerId}"]`))
+  const card = document.querySelector(`[data-persona-id="${event.currentTarget.dataset.markerId}"]`);
+  // Then we toggle the class "highlight github" to the card
+  card.classList.toggle('highlight');
+}
 
 export { initMapbox };
